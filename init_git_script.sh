f7a63b508ddeb2394bc8ad724b44b36fba0957ba
nicc <>
2022-09-22 12:51:13 +0200

    no message

--- init_git_script.sh
+++
@@ -1,6 +0,0 @@
-git remote add myrepo https://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@github.com/niccolocorsani/Progetto-di-tesi.git
-git add .
-git commit -m "Messaggio per la commit"
-git push myrepo master
-
-
